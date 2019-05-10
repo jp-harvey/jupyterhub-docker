9ba8e7e027c71308de3a77ed799307925100f74b
jp-harvey <jpharvey@cloudquarterback.com>
2019-05-10 09:47:05 -0700

    Not working

--- jupyterhub/jupyterhub_config.py
+++ jupyterhub/jupyterhub_config.py
@@ -7,26 +7,9 @@ from oauthenticator.oauth2 import OAuthLoginHandler
 from oauthenticator.generic import GenericOAuthenticator
 from tornado.auth import OAuth2Mixin
 
-class UVSQMixin(OAuth2Mixin):
-    _OAUTH_AUTHORIZE_URL = 'https://jupyter.ens.uvsq.fr/c2o2b/login'
-    _OAUTH_ACCESS_TOKEN_URL = 'https://jupyter.ens.uvsq.fr/c2o2b/token'
+c.JupyterHub.authenticator_class = 'jhub_remote_user_authenticator.remote_user_auth.RemoteUserAuthenticator'
 
-class UVSQLoginHandler(OAuthLoginHandler, UVSQMixin):
-    pass
-
-class UVSQAuthenticator(GenericOAuthenticator):
-    login_service = 'UVSQ'
-    login_handler = UVSQLoginHandler
-    client_id = '0'
-    client_secret = ''
-    userdata_url = 'https://jupyter.ens.uvsq.fr/c2o2b/userdata'
-    token_url = 'https://jupyter.ens.uvsq.fr/c2o2b/token'
-    oauth_callback_url = 'https://jupyter.ens.uvsq.fr/hub/oauth_callback'
-
-c.JupyterHub.authenticator_class = UVSQAuthenticator
-
-c.Authenticator.admin_users = { 'lucadefe' }
-
+c.Authenticator.admin_users = {'admin', 'jptest'}
 
 ## Docker spawner
 import os
@@ -34,8 +17,19 @@ import os
 c.JupyterHub.spawner_class = 'dockerspawner.DockerSpawner'
 c.DockerSpawner.image = os.environ['DOCKER_JUPYTER_CONTAINER']
 c.DockerSpawner.network_name = os.environ['DOCKER_NETWORK_NAME']
+
+origin = '*'
+c.JupyterHub.tornado_settings = {
+    'headers': {
+      'Access-Control-Allow-Origin': origin,
+   },
+}
+c.Spawner.args = ['--NotebookApp.allow_origin={0}'.format(origin)]
+
 # See https://github.com/jupyterhub/dockerspawner/blob/master/examples/oauth/jupyterhub_config.py
 c.JupyterHub.hub_ip = os.environ['HUB_IP']
+
+c.NotebookApp.terminals_enabled = False
 
 # user data persistence
 # see https://github.com/jupyterhub/dockerspawner#data-persistence-and-dockerspawner
